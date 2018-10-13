d4b30bb27c327fb074bdc1fbb40492dce1ea87bd
anishchandak7 <chandakanish7@gmail.com>
2018-10-14 01:33:41 +0530

    different ways of printing hello world

---
+++ Python/HELLO_WORLD1.py
@@ -0,0 +1,12 @@
+# print hello world in python
+
+print ("Hello world")
+
+# print hello world 5 times
+print("hello world"*5)
+
+#print hello world using variables
+
+word = "hello world"
+
+print(word)
